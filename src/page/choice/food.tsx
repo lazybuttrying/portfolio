a7a8fa782e07f768a1591c4da1b3ecaf4dceec74
김도연 <finallazy9999@gmail.com>
2022-06-12 01:38:24 +0900

    use html dialog

--- src/page/choice/food.tsx
+++ src/page/choice/food.tsx
@@ -1,43 +1,46 @@
 import { FOOD } from "../../data/food";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Ul } from "../../layout/component/list";
 
 export default function Food() {
-  const [idx, setIdx] = useState(0);
+  // const [idx, setIdx] = useState(0);
+  const idx = useRef<number>(0);
   const [value, setValue] = useState("");
   const [result, setResult] = useState("");
   const navigate = useNavigate();
 
   return (
     <>
-      <h4>{FOOD.question[idx].question}</h4>
+      <h4>{FOOD.question[idx.current].question}</h4>
 
       <form>
         <Ul>
-          {FOOD.question[idx].options.map((option: string, index: number) => {
-            return (
-              <li key={option}>
-                <input
-                  type="radio"
-                  name="option"
-                  value={index}
-                  onChange={(e) => setValue(e.target.value)}
-                />
-                <label htmlFor="option">{option}</label>
-              </li>
-            );
-          })}
+          {FOOD.question[idx.current].options.map(
+            (option: string, index: number) => {
+              return (
+                <li key={option}>
+                  <input
+                    type="radio"
+                    name="option"
+                    value={index}
+                    onChange={(e) => setValue(e.target.value)}
+                  />
+                  <label htmlFor="option">{option}</label>
+                </li>
+              );
+            }
+          )}
         </Ul>
         <button
           type="button"
           onClick={(e) => {
-            if (idx === FOOD.question.length - 1) {
+            if (idx.current === FOOD.question.length - 1) {
               navigate("/quiz/food/result", { state: result + value });
               return;
             }
             setResult(result + value);
-            setIdx(idx + 1);
+            idx.current += 1;
           }}
         >
           다음으로
--- src/page/choice/result.tsx
+++ src/page/choice/result.tsx
@@ -1,10 +1,41 @@
 import { FOOD } from "../../data/food";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { Ul } from "../../layout/component/list";
+import styled from "@emotion/styled";
+
+const dialog = (dialogRef: any) => {
+  return (
+    <dialog ref={dialogRef}>
+      <form method="dialog">
+        <p>
+          <label>
+            음식 기입:
+            <input type="text" />
+          </label>
+        </p>
+        <p>
+          <label>
+            등록된 음식
+            <select>
+              <option>선택</option>
+              <option>치킨</option>
+            </select>
+          </label>
+        </p>
+        <menu>
+          <button value="cancel">취소</button>
+          <button id="confirmBtn" value="default">
+            확인
+          </button>
+        </menu>
+      </form>
+    </dialog>
+  );
+};
 
 export default function Result() {
   const [idx, setIdx] = useState(0);
+  const dialogRef = useRef<HTMLDialogElement>(null);
 
   const navigate = useNavigate();
   const { state } = useLocation();
@@ -18,8 +49,19 @@ export default function Result() {
 
   return (
     <>
+      {dialog(dialogRef)}
       <h3>선호하는 음식</h3>
-      <h4>{food ? food : "그런 종류는..."}</h4>
+      <h4>
+        {food ? food : "그런 종류는..."} <br />
+        <button
+          type="button"
+          onClick={() => {
+            dialogRef.current?.showModal();
+          }}
+        >
+          음식 추가하기
+        </button>
+      </h4>
 
       <h3>선호하는 키워드</h3>
       <h4>{msg}</h4>
